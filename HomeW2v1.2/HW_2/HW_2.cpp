6cbc1fafb6352dbefa3fdbd0474b8f3aab5dee47
Antony_777 <61995096+Alex777-byte@users.noreply.github.com>
2021-08-03 12:32:46 +0300

    HW_2
    Домашняя работа к видеоуроку и игра в крестики нолики в одном решении. Игра требует доработок

---
+++ HomeW2v1.2/HW_2/HW_2.cpp
@@ -0,0 +1,426 @@
+﻿#include <iostream>
+#include <cstring>
+#include <string.h>
+
+using namespace std; // нужно для идентификации string наряду с библиотекой
+//типы переменных, задание 1
+short int a = 5;
+int b = 5;
+long long c = 1'000'000;
+char chLength = 'm';
+bool d = true;
+float e = 9.87654321f;
+double massEarth = 5e24;
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+// Перечисление, задание 2, массив задание 3
+
+
+enum Game
+{
+    Symbol_X = 'X',
+    Symbol_O = '0',
+    Field0 = '-',  // пустые ячейки
+    Field1 = '|'   // границы между ячейками
+};
+
+
+int task2_3()
+{
+    Game Symbols[2] = { Symbol_X, Symbol_O };
+    Game Fields[2] = { Field0, Field1 };
+    std::cout << (char)Symbols[0] << ", " << (char)Symbols[1] << std::endl;
+    std::cout << (char)Fields[0] << ", " << (char)Fields[1] << std::endl;
+    return 0;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+//задание 4*
+
+enum PlayerMove { Player1ID, Player2ID };
+
+struct Play
+{
+    string EmptyCell;
+    string Field0 = "-";
+    string Field1 = "|";
+    //PlayerMove Player1ID, Player2ID;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------
+//задание 5**
+//-------------------------------------------------------------------------------------------------------------------------
+
+union MyVariantData
+{
+    int DataInt;
+    float DataFloat;
+    char DataChar;
+};
+
+struct MyData
+{
+    MyVariantData data;
+
+    unsigned int isInt : 1;
+    unsigned int isFloat : 1;
+    unsigned int isChar : 1;
+};
+
+int task5()
+{
+    MyData var;
+
+    var.data.DataFloat = 77.77f;
+    var.isInt = 0;
+    var.isFloat = 1; //храним float
+    var.isChar = 0;
+    std::cout << var.isInt << ", " << var.isInt << ", " <<var.isChar<< "\n" << std::endl;
+    return 0;
+}
+
+
+
+int main()
+{
+    task2_3();
+    task5();
+    return 0;
+}
+    /*
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        std::cout << positions[i][j];
+    }
+    std::cout << "\n";
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        std::cout << positions[i + 1][j];
+    }
+    std::cout << "\n";
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        std::cout << positions[i + 2][j];
+    }
+    std::cout << "\n";
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        std::cout << positions[i + 3][j];
+    }
+    std::cout << "\n";
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        std::cout << positions[i + 4][j];
+    }
+    std::cout << "\n";
+
+
+    int inputSF;
+    char inputSS;
+    std::cout << "Введите значение ячейки, в которую хотите записать значение";
+    std::cin >> inputSF;
+    switch (inputSF)
+    {
+    case (0):
+            std::cout << "Введите то, что хотите записать, крестик или нолик";
+            std::cin >> inputSS;
+        if (inputSS == 'X')
+        {
+            positions[4][0] = inputSS;
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 1][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 2][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 3][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 4][j];
+            }
+        }
+        else if(inputSS == 'O')
+        {
+            inputSS = 'O';
+            positions[4][0] = inputSS;
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 1][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 2][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 3][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 4][j];
+            }
+            
+        }
+        break;
+      case (1):
+        std::cout << "Введите то, что хотите записать, крестик или нолик";
+        std::cin >> inputSS;
+        if (inputSS == 'X')
+        {
+            positions[4][2] = inputSS;
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 1][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 2][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 3][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 4][j];
+            }
+        }
+        else if(inputSS == 'O')
+        {
+            positions[4][2] = inputSS;
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 1][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 2][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 3][j];
+            }
+            std::cout << "\n";
+            for (int j = 0; j < 5; j++)
+            {
+                int i = 0;
+                std::cout << positions[i + 4][j];
+            }
+        }
+        break;
+      case (2):
+          std::cout << "Введите то, что хотите записать, крестик или нолик";
+          std::cin >> inputSS;
+          if (inputSS == 'X')
+          {
+              positions[4][5] = inputSS;
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 1][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 2][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 3][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 4][j];
+              }
+          }
+          else if (inputSS == 'O')
+          {
+              positions[4][5] = inputSS;
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 1][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 2][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 3][j];
+              }
+              std::cout << "\n";
+              for (int j = 0; j < 5; j++)
+              {
+                  int i = 0;
+                  std::cout << positions[i + 4][j];
+              }
+          }
+          break;
+    default:
+        std::cout << "Unknown";
+        break;
+    }
+    return 0;
+}*/
+
+    //Game Symbol = static_cast<Game>(inputSymbol)
+    
+
+    //Switch(Playercell)
+   // {
+   //     case Playercell = 0:
+   //         positions[0][0] = X
+   //         std::cout << 
+
+   // }
+    //std::cin >> EmptyCell;
+    //int i = 1;
+    //int g = 0;
+    //char positions[5][5];
+   // (char)positions[i+1][g+1] = "|";
+   
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+//игра в крестики нолики
+//-------------------------------------------------------------------------------------------------------------------------
+
+
+
+/*
+#include <iostream>//стандартная библиотека
+
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(0, "");
+
+
+    char positions[9] = { '-','-','-','-','-','-','-','-','-', };
+
+    int x;
+    int o;
+
+    cout << "Номера клеток:" << endl;
+    cout << " 1 " <<'|' << " 2 " << '|' << " 3 " << "|" << endl;
+    cout << " 4 " << '|'<< " 5 " << '|' << " 6 " << "|" << endl;
+    cout << " 7 " << '|' <<  " 8 " << '|' << " 9 " << "|" << "\n" << endl;
+
+
+    cout << "Игровое поле (--- пусто):" << endl;
+    cout << positions[0] << '|' << positions[1] << '|' << positions[2] << endl;
+    cout << positions[3] << '|' << positions[4] << '|' << positions[5] <<  endl;
+    cout << positions[6] << '|' << positions[7] << '|' << positions[8] << "\n" << endl;
+
+   for (int o = 0; o <= 9; ++o) {
+        cout << "Введите номер клетки, куда ходить  X: ";
+        cin >> x;
+        positions[x - 1] = 'x';
+
+        cout << positions[0] << '|' << positions[1] << '|' << positions[2] << "|" << endl;
+        cout << positions[3] << '|' << positions[4] << '|' << positions[5] << "|" << endl;
+        cout << positions[6] << '|' << positions[7] << '|' << positions[8] << "|" << "\n" << endl;
+
+
+
+        cout << "Введите номер клетки, куда ходить 0: ";
+        cin >> o;
+        positions[o - 1] = '0';
+
+        cout << positions[0] << '|' << positions[1] << '|' << positions[2] << "|" << endl;
+        cout << positions[3] << '|' << positions[4] << '|' << positions[5] << "|" << endl;
+        cout << positions[6] << '|' << positions[7] << '|' << positions[8] << "|" << "\n" << endl;
+
+
+    }
+
+    system("pause");
+    return 0;
+
+
+}
+*/
---
+++ HomeW2v1.2/TicToc/TicToc.cpp
@@ -0,0 +1,77 @@
+﻿// TicToc.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <cstring>
+#include <string.h>
+#include "TicToc.h"
+
+using namespace std; // нужно для идентификации string наряду с библиотекой
+
+#define  SIZE 3
+
+string positions[3][3] = { " "," "," "," "," "," "," "," "," ", };
+int     inputSF;
+char    inputSS;
+string  currentSymbol = "X";
+
+int X;
+int Y;
+
+void changeSymbol()
+{
+    if (currentSymbol == "X")
+        currentSymbol = "0";
+    else
+        currentSymbol = "X";
+}
+
+void PrintGameField()
+{
+    cout << "Игровое поле (--- пусто):" << endl;
+    for (int i = 0;i < SIZE;i++)
+    {
+        for (int j = 0;j < SIZE;j++)
+        {
+            cout << positions[i][j] << " | ";
+        }
+        cout << endl;
+    }
+}
+
+void inputPleer()
+{
+    cout << "Введите координату X:";
+    cin >> X;
+    cout << "Введите координату Y:";
+    cin >> Y;
+//    if(X< SIZE)
+//    if(positions[X][Y]== " ")
+    positions[X][Y] = currentSymbol;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");    // подключаем русский язык
+    PrintGameField();
+
+    for (int q = 0; q <= 9; ++q)
+    {
+        inputPleer();
+        changeSymbol();
+        PrintGameField();
+    }
+    return 0;
+}
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
